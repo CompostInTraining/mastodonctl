82a43d204c1369dc770b4589def194a553775c5b
prplecake <me@prplecake.com>
2022-11-14 20:00:54 -0600

    Add mastodon-update.sh

---
+++ mastodon-update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S bash -xe
+
+sudo -u mastodon zsh <<"EOF"
+export RAILS_ENV=production
+cd /home/mastodon/live
+source /home/mastodon/.zshrc
+git pull
+bundle install
+yarn install
+SKIP_POST_DEPLOYMENT_MIGRATIONS=true bundle exec rails db:migrate
+bundle exec rails assets:precompile
+EOF
+
+sudo systemctl reload mastodon-web.service
+sudo systemctl restart mastodon-sidekiq.service mastodon-streaming.service
+sudo systemctl start elasticsearch
+
+sudo -u mastodon zsh <<"EOF"
+export RAILS_ENV=production
+cd /home/mastodon/live
+source /home/mastodon/.zshrc
+./bin/tootctl cache clear
+bundle exec rails db:migrate
+./bin/tootctl feeds clear
+./bin/tootctl feeds build
+EOF
