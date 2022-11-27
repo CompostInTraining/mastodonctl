b0c92a7fd2bc3dc57e8aeb27b2ceb64af166622c
prplecake <me@prplecake.com>
2022-11-27 10:12:20 -0600

    Save config as well, but only on Sundays

--- mastodon-backup.sh
+++ mastodon-backup.sh
@@ -8,4 +8,14 @@ now=$(date --iso-8601=seconds)
 echo "Postgres..."
 cd ~ && pg_dump -Fc mastodon_production -f postgres.dump
 aws s3 cp /home/mastodon/postgres.dump s3://$S3_BUCKET_NAME/backup-postgres/$now/
+
+# config...on sundays
+if [ "$(date +%u)" == "7" ]; then
+  echo "Config..."
+  cd ~
+  age -o .env.production.age -R $BACKUP_RECIPIENTS live/.env.production
+  aws s3 cp /home/mastodon/.env.production.age s3://$S3_BUCKET_NAME/backup-config/$now/
+fi
+
+# heartbeat
 curl $HEART_BEAT_URL
