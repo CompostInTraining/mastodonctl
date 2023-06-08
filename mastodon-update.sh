9ef3d8ddb70d6a69dc3254917afa7609a5d72a10
prplecake <me@prplecake.com>
2023-06-08 18:40:42 -0500

    Add lftp command

--- mastodon-update.sh
+++ mastodon-update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S bash -e
 
+ROOTDIR=$(dirname "$(readlink -f "$0")")
+source "$ROOTDIR/.env"
+
 sudo -u mastodon zsh <<"EOF"
 export RAILS_ENV=production
 cd /home/mastodon/live
@@ -9,6 +12,10 @@ bundle install
 yarn install --pure-lockfile
 SKIP_POST_DEPLOYMENT_MIGRATIONS=true bundle exec rails db:migrate
 bundle exec rails assets:precompile
+EOF
+
+lftp -u $ASSETS_FTP_USER,"$ASSETS_FTP_PASS" $ASSETS_FTP_HOST <<EOF
+mirror -R -P20 -e /home/mastodon/live/public/* .
 EOF
 
 sudo systemctl reload mastodon-web.service
