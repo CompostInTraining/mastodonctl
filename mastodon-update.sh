b200366ee1344a08a25d7cff8d4f097bbb92065f
Matty Jorgensen <me@prplecake.com>
2023-08-20 01:58:53 -0500

    Increase verbosity

--- mastodon-update.sh
+++ mastodon-update.sh
@@ -7,10 +7,13 @@ source /home/mastodon/.zshrc
 git pull
 bundle install
 yarn install --pure-lockfile
+echo Running pre-deployment migrations...
 SKIP_POST_DEPLOYMENT_MIGRATIONS=true bundle exec rails db:migrate
+echo Compiling assets...
 bundle exec rails assets:precompile
 EOF
 
+echo Restarting services...
 sudo systemctl reload mastodon-web.service
 sudo systemctl restart mastodon-sidekiq.service mastodon-streaming.service
 # Uncomment following line if you run ES on the same server as Mastodon.
@@ -20,8 +23,11 @@ sudo -u mastodon zsh <<"EOF"
 export RAILS_ENV=production
 cd /home/mastodon/live
 source /home/mastodon/.zshrc
+echo Clearing cache...
 ./bin/tootctl cache clear
+echo Running post-deployment migrations...
 bundle exec rails db:migrate
+echo Rebuilding feeds...
 ./bin/tootctl feeds clear
 ./bin/tootctl feeds build
 EOF
